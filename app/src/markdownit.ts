edc5793ee0e1eee26daabcc8982cf1f3bcc3b9b3
LiamSnow <mail@liamsnow.com>
2024-04-24 22:14:42 -0400

    Added MarkdownIt Extensions

--- app/src/markdownit.ts
+++ app/src/markdownit.ts
@@ -3,10 +3,16 @@ import { parseArgs } from 'https://deno.land/std@0.217.0/cli/parse_args.ts';
 import { default as highlight } from 'https://cdn.skypack.dev/highlight.js@11.9.0';
 // @deno-types="https://esm.sh/v135/@types/markdown-it@13.0.7/index.d.ts";
 import MarkdownIt from 'https://esm.sh/markdown-it@14.0.0';
+
 import { full as MarkdownItEmoji } from 'https://esm.sh/markdown-it-emoji@3.0.0';
 import { default as MarkdownItFootnote } from 'https://esm.sh/markdown-it-footnote@4.0.0';
+import { default as MarkdownItSub } from 'https://esm.sh/markdown-it-sub@2.0.0';
+import { default as MarkdownItSup } from 'https://esm.sh/markdown-it-sup@2.0.0';
+import { default as MarkdownItIns } from 'https://esm.sh/markdown-it-ins@4.0.0';
+import { default as MarkdownItMark } from 'https://esm.sh/markdown-it-mark@4.0.0';
 import { default as MarkdownItTaskLists } from 'https://esm.sh/markdown-it-task-lists@2.1.1';
 import { default as MarkdownItTexmath } from 'https://esm.sh/markdown-it-texmath@1.0.0';
+
 import Katex from 'https://esm.sh/katex@0.16.9';
 
 const __args = parseArgs(Deno.args);
@@ -29,6 +35,10 @@ const md = new MarkdownIt('default', {
   }),
 }).use(MarkdownItEmoji)
   .use(MarkdownItFootnote)
+  .use(MarkdownItSub)
+  .use(MarkdownItSup)
+  .use(MarkdownItIns)
+  .use(MarkdownItMark)
   .use(MarkdownItTaskLists, { enabled: false, label: true })
   .use(MarkdownItTexmath, {
     engine: Katex,
@@ -38,7 +48,152 @@ const md = new MarkdownIt('default', {
       strict: false,
       throwOnError: false,
     },
-  });
+  })
+  .use(MarkdownItFrontMatter);
+
+// Credit to parksb
+function MarkdownItFrontMatter(md, cb) {
+  var min_markers = 3,
+      marker_str  = '-',
+      marker_char = marker_str.charCodeAt(0),
+      marker_len  = marker_str.length;
+
+  function frontMatter(state, startLine, endLine, silent) {
+    var pos,
+        nextLine,
+        marker_count,
+        token,
+        old_parent,
+        old_line_max,
+        start_content,
+        auto_closed = false,
+        start = state.bMarks[startLine] + state.tShift[startLine],
+        max = state.eMarks[startLine];
+
+    // Check out the first character of the first line quickly,
+    // this should filter out non-front matter
+    if (startLine !== 0 || marker_char !== state.src.charCodeAt(0)) {
+      return false;
+    }
+
+    // Check out the rest of the marker string
+    // while pos <= 3
+    for (pos = start + 1; pos <= max; pos++) {
+      if (marker_str[(pos - start) % marker_len] !== state.src[pos]) {
+        start_content = pos + 1;
+        break;
+      }
+    }
+
+    marker_count = Math.floor((pos - start) / marker_len);
+
+    if (marker_count < min_markers) {
+      return false;
+    }
+    pos -= (pos - start) % marker_len;
+
+    // Since start is found, we can report success here in validation mode
+    if (silent) {
+      return true;
+    }
+
+    // Search for the end of the block
+    nextLine = startLine;
+
+    for (;;) {
+      nextLine++;
+      if (nextLine >= endLine) {
+        // unclosed block should be autoclosed by end of document.
+        // also block seems to be autoclosed by end of parent
+        break;
+      }
+
+      if (state.src.slice(start, max) === '...') {
+        break;
+      }
+
+      start = state.bMarks[nextLine] + state.tShift[nextLine];
+      max = state.eMarks[nextLine];
+
+      if (start < max && state.sCount[nextLine] < state.blkIndent) {
+        // non-empty line with negative indent should stop the list:
+        // - ```
+        //  test
+        break;
+      }
+
+      if (marker_char !== state.src.charCodeAt(start)) {
+        continue;
+      }
+
+      if (state.sCount[nextLine] - state.blkIndent >= 4) {
+        // closing fence should be indented less than 4 spaces
+        continue;
+      }
+
+      for (pos = start + 1; pos <= max; pos++) {
+        if (marker_str[(pos - start) % marker_len] !== state.src[pos]) {
+          break;
+        }
+      }
+
+      // closing code fence must be at least as long as the opening one
+      if (Math.floor((pos - start) / marker_len) < marker_count) {
+        continue;
+      }
+
+      // make sure tail has spaces only
+      pos -= (pos - start) % marker_len;
+      pos = state.skipSpaces(pos);
+
+      if (pos < max) {
+        continue;
+      }
+
+      // found!
+      auto_closed = true;
+      break;
+    }
+
+    old_parent = state.parentType;
+    old_line_max = state.lineMax;
+    state.parentType = 'container';
+
+    // this will prevent lazy continuations from ever going past our end marker
+    state.lineMax = nextLine;
+
+    token        = state.push('front_matter', null, 0);
+    token.hidden = true;
+    token.markup = state.src.slice(startLine, pos);
+    token.block  = true;
+    token.map    = [ startLine, nextLine + (auto_closed ? 1 : 0) ];
+    token.meta   = state.src.slice(start_content, start - 1);
+
+    state.parentType = old_parent;
+    state.lineMax = old_line_max;
+    state.line = nextLine + (auto_closed ? 1 : 0);
+
+    if (typeof cb === 'function') {
+      cb(token.meta);
+    }
+
+    return true;
+  }
+
+  md.block.ruler.before(
+    'table',
+    'front_matter',
+    frontMatter,
+    {
+      alt: [
+        'paragraph',
+        'reference',
+        'blockquote',
+        'list'
+      ]
+    }
+  );
+};
 
 md.renderer.rules.link_open = (tokens, idx, options) => {
   const token = tokens[idx];
